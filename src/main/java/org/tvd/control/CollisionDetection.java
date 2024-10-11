0d228b0c6a6b5f814ef254d1656ee3041008a9be
trandung09 <trandung09082004@gmail.com>
2024-10-11 15:59:31 +0700

    Update collision.

--- src/main/java/org/tvd/control/CollisionDetection.java
+++ src/main/java/org/tvd/control/CollisionDetection.java
@@ -1,4 +1,13 @@
 package org.tvd.control;
 
+import org.tvd.frame.GamePanel;
+
 public class CollisionDetection {
+
+    private final GamePanel gamePanel;
+
+    public CollisionDetection(GamePanel gamePanel) {
+
+        this.gamePanel = gamePanel;
+    }
 }
