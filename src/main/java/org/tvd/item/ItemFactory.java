556be91c13a969046923214a354b1792e3d75962
trandung09 <trandung09082004@gmail.com>
2024-10-14 01:39:13 +0700

    Item factory.

---
+++ src/main/java/org/tvd/item/ItemFactory.java
@@ -0,0 +1,36 @@
+package org.tvd.item;
+
+import org.tvd.frame.GamePanel;
+
+public class ItemFactory {
+
+    private ItemFactory() {
+    }
+
+    public SuperItem getItem(String name, GamePanel gamePanel) {
+
+        if (name == null || gamePanel == null) {
+            throw new IllegalArgumentException("name or gamePanel is null");
+        }
+
+        return switch (name.toLowerCase()) {
+            case "boots" -> new Boots(gamePanel);
+            case "coin" -> new Coin(gamePanel);
+            case "diamond" -> new Diamond(gamePanel);
+            case "key" -> new Key(gamePanel);
+            case "lantern" -> new Lantern(gamePanel);
+            case "tent" -> new Tent(gamePanel);
+            default -> throw new IllegalStateException("Unexpected value: " + name.toLowerCase());
+        };
+    }
+
+    public static ItemFactory getInstance() {
+        return Singleton.INSTANCE;
+    }
+
+    private static class Singleton {
+
+        // Instance of ItemFactory
+        private static final ItemFactory INSTANCE = new ItemFactory();
+    }
+}
