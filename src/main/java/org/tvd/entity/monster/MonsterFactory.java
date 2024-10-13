5e3e453b3d426b7e8be9ae398510ee1b55f6318c
trandung09 <trandung09082004@gmail.com>
2024-10-14 01:39:40 +0700

    Monster factory.

---
+++ src/main/java/org/tvd/entity/monster/MonsterFactory.java
@@ -0,0 +1,33 @@
+package org.tvd.entity.monster;
+
+import org.tvd.frame.GamePanel;
+
+public class MonsterFactory {
+
+    private MonsterFactory() {
+    }
+
+    public Monster getMonster(String name, GamePanel gamePanel) {
+
+        if (name == null || gamePanel == null) {
+            throw new IllegalArgumentException("Monster name or gamePanel is null");
+        }
+
+        return switch (name.toLowerCase()) {
+            case "bat" -> new Bat(gamePanel);
+            case "skeleton lord" -> new SkeletonLord(gamePanel);
+            case "slimey" -> new Slimey(gamePanel);
+            case "orc" -> new Orc(gamePanel);
+            default -> throw new IllegalStateException("Unexpected value: " + name.toLowerCase());
+        };
+    }
+
+    public static MonsterFactory getInstance() {
+        return Singleton.INSTANCE;
+    }
+
+    private static class Singleton {
+        // Instance of MonsterFactory
+        private static final MonsterFactory INSTANCE = new MonsterFactory();
+    }
+}
