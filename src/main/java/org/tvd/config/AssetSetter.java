6349a81f9abef57985be9befd741675e7f32abac
trandung09 <trandung09082004@gmail.com>
2024-10-13 22:45:33 +0700

    Update asset setter.

--- src/main/java/org/tvd/config/AssetSetter.java
+++ src/main/java/org/tvd/config/AssetSetter.java
@@ -1,4 +1,17 @@
 package org.tvd.config;
 
+import org.tvd.entity.monster.Monster;
+import org.tvd.item.SuperItem;
+
+import java.util.ArrayList;
+
 public class AssetSetter {
+
+    public static void loadAllItem(int level, ArrayList<SuperItem> items) {
+
+    }
+
+    public static void loadAllMonster(int level, ArrayList<Monster> monsters) {
+
+    }
 }
