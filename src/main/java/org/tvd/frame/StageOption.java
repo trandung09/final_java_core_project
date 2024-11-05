b685abcba15962a90930500e4810b68c22dbfe72
trandung09 <trandung09082004@gmail.com>
2024-11-05 11:48:04 +0700

    Update.

---
+++ src/main/java/org/tvd/frame/StageOption.java
@@ -0,0 +1,9 @@
+package org.tvd.frame;
+
+public class StageOption {
+
+    public static Menu MenuStage = Menu.NEW_GAME;
+    public static Menu PauseStage = Menu.CONTINUE;
+    public static Menu OverStage = Menu.RETRY;
+    public static Menu WinStage = Menu.NEW_GAME;
+}
