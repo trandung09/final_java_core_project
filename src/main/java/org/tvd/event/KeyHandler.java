f7385ac4744567e34f19eae1635162cd8cdedc5f
trandung09 <trandung09082004@gmail.com>
2024-11-05 11:46:08 +0700

    Separate methods corresponding to game states.

--- src/main/java/org/tvd/event/KeyHandler.java
+++ src/main/java/org/tvd/event/KeyHandler.java
@@ -1,6 +1,9 @@
 package org.tvd.event;
 
 import org.tvd.frame.GamePanel;
+import org.tvd.frame.GameStatus;
+import org.tvd.frame.Menu;
+import org.tvd.frame.StageOption;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -18,29 +21,12 @@ public class KeyHandler implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
 
-        int code = e.getKeyCode();
+        int keyCode = e.getKeyCode();
 
-        switch (code) {
-            case KeyEvent.VK_A:
-                pressed.left = true;
-                break;
-            case KeyEvent.VK_D:
-                pressed.right = true;
-                break;
-            case KeyEvent.VK_W:
-                pressed.up = true;
-                break;
-            case KeyEvent.VK_S:
-                pressed.down = true;
-                break;
-            case KeyEvent.VK_L:
-                pressed.lighting = !pressed.lighting;
-                break;
-            case KeyEvent.VK_ENTER:
-                pressed.enter = true;
-                break;
-            default:
-                throw new IllegalArgumentException("Unsupported key code: " + code);
+        switch (gamePanel.gameStatus) {
+            case GAME_MENU -> keyHandlerMenuGameStage(keyCode);
+            case GAME_RUNNING -> keyHandlerRunningGameStage(keyCode);
+            case GAME_PAUSE -> keyHandlerPauseGameStage(keyCode);
         }
     }
 
@@ -65,11 +51,60 @@ public class KeyHandler implements KeyListener {
             case KeyEvent.VK_L:
                 break;
             case KeyEvent.VK_ENTER:
-                System.out.println(pressed.enter);
                 pressed.enter = false;
                 break;
             default:
-                throw new IllegalArgumentException("Unsupported key code: " + code);
+        }
+    }
+
+    private void keyHandlerMenuGameStage(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.VK_ENTER -> {
+                if (StageOption.MenuStage == Menu.QUIT) System.exit(0);
+                if (StageOption.MenuStage == Menu.NEW_GAME) gamePanel.gameStatus = GameStatus.GAME_RUNNING;
+            }
+            case KeyEvent.VK_UP -> {
+                if (StageOption.MenuStage == Menu.NEW_GAME) StageOption.MenuStage = Menu.QUIT;
+                else if (StageOption.MenuStage == Menu.ABOUT) StageOption.MenuStage = Menu.NEW_GAME;
+                else if (StageOption.MenuStage == Menu.QUIT) StageOption.MenuStage = Menu.ABOUT;
+            }
+            case KeyEvent.VK_DOWN -> {
+                if (StageOption.MenuStage == Menu.NEW_GAME) StageOption.MenuStage = Menu.ABOUT;
+                else if (StageOption.MenuStage == Menu.ABOUT) StageOption.MenuStage = Menu.QUIT;
+                else if (StageOption.MenuStage == Menu.QUIT) StageOption.MenuStage = Menu.NEW_GAME;
+            }
+        }
+    }
+
+    private void keyHandlerRunningGameStage(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.VK_W -> pressed.up = true;
+            case KeyEvent.VK_S -> pressed.down = true;
+            case KeyEvent.VK_A -> pressed.left = true;
+            case KeyEvent.VK_D -> pressed.right = true;
+            case KeyEvent.VK_P -> {
+                if (gamePanel.gameStatus == GameStatus.GAME_RUNNING) {
+                    gamePanel.gameStatus = GameStatus.GAME_PAUSE;
+                }
+            }
+            case KeyEvent.VK_L -> pressed.lighting = !pressed.lighting;
+            default -> {}
+        }
+    }
+
+    private void keyHandlerPauseGameStage(int keyCode) {
+        switch (keyCode) {
+            case KeyEvent.VK_ENTER -> {
+                if (StageOption.PauseStage == Menu.CONTINUE) gamePanel.gameStatus = GameStatus.GAME_RUNNING;
+                else if (StageOption.PauseStage == Menu.QUIT) {
+                    StageOption.PauseStage = Menu.CONTINUE;
+                    gamePanel.gameStatus = GameStatus.GAME_MENU;
+                }
+            }
+            case KeyEvent.VK_UP, KeyEvent.VK_DOWN -> {
+                if (StageOption.PauseStage == Menu.CONTINUE) StageOption.PauseStage = Menu.QUIT;
+                else if (StageOption.PauseStage == Menu.QUIT) StageOption.PauseStage = Menu.CONTINUE;
+            }
         }
     }
 
