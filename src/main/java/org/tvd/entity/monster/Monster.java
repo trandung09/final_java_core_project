ed27a4badf64e9eea4c850954f222e37c69f23d5
trandung09 <trandung09082004@gmail.com>
2024-11-13 17:08:24 +0700

    Update: render method for default action.

--- src/main/java/org/tvd/entity/monster/Monster.java
+++ src/main/java/org/tvd/entity/monster/Monster.java
@@ -1,11 +1,14 @@
 package org.tvd.entity.monster;
 
+import org.tvd.asset.FrameAsset;
 import org.tvd.entity.Direction;
 import org.tvd.entity.Entity;
 import org.tvd.entity.EntityActions;
 import org.tvd.frame.GamePanel;
 
-import java.awt.*;
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.util.Random;
 
 public abstract class Monster extends Entity implements EntityActions {
@@ -20,19 +23,19 @@ public abstract class Monster extends Entity implements EntityActions {
     }
 
     /**
-     * Random hướng di chuyển của quái vật, cập nhật hoạt ảnh quái vật
+     * Randomize monster movement direction, update monster animation
      */
     @Override
     public void setAction() {
 
-        // Cập nhật hình ảnh quái vật
+        // Update monster animation
         counter.drawCounter++;
         if (counter.drawCounter == 15) {
             counter.drawCounter = 0;
             imageChecker = !imageChecker;
         }
 
-        // Random hướng di chuyển của quái vật
+        // Randomize monster movement direction
         counter.actionCounter++;
         if (counter.actionCounter == 150) {
 
@@ -48,9 +51,9 @@ public abstract class Monster extends Entity implements EntityActions {
     }
 
     /**
-     * Sửa lại action của qúai vật khi bị người chơi tấn công
-     * + Làm cho quái vật ra xa khỏi người chơi khi bị gây sát thương
-     * + Cập nhật hướng của quái vật quay về phía hướng người chơi
+     * Fixed monster actions when attacked by player
+     * + Makes monsters move away from the player when damaged
+     * + Update the monster's direction facing the player
      */
     @Override
     public void resetAction() {
@@ -64,7 +67,7 @@ public abstract class Monster extends Entity implements EntityActions {
             case RIGHT -> direction = Direction.LEFT;
         }
 
-        // Sửa vị trí quái vật khi bị người chơi gây sát thương
+        // Fix monster position when player damages
         if (!isCollisionOn) {
             switch (direction) {
                 case DOWN -> worldY -= speed * 15;
@@ -73,12 +76,6 @@ public abstract class Monster extends Entity implements EntityActions {
                 case RIGHT ->  worldX -= speed * 15;
             }
         }
-    }
-
-    @Override
-    public void defending() {
-
-        isInvincible = true;
     }
 
     @Override
@@ -92,20 +89,89 @@ public abstract class Monster extends Entity implements EntityActions {
 
     }
 
+    @Override
+    public void defending() {
+
+        isInvincible = true;
+    }
+
     /**
-     * Sử dụng thuật toán A star tìm kiếm đường đi tới vị trí của player
+     * Use A star algorithm to find the path to the player's position
      */
     public void findPathToPlayer() {
 
     }
 
     @Override
-    public void render(Graphics2D g2d) {
+    public void update() {
 
+        moving();
+
+        if (counter.drawCounter++ > 10) {
+            counter.drawCounter = 0;
+            imageChecker = !imageChecker;
+        }
+
+        if (isHpBarOn) {
+
+            if (counter.hpBarCounter++ > 500) {
+                isHpBarOn = false;
+                counter.hpBarCounter = 0;
+            }
+        }
+
+        if (isInvincible) {
+
+            isHpBarOn = true;
+
+            if (counter.invincibleCounter++ > 60) {
+                isInvincible = false;
+                counter.invincibleCounter = 0;
+            }
+        }
     }
 
     @Override
-    public void update() {
-        
+    public void render(Graphics2D g2d) {
+
+        int screenX = worldX - gamePanel.player.getWorldX() + gamePanel.player.screenX;
+        int screenY = worldY - gamePanel.player.getWorldY() + gamePanel.player.screenY;
+
+        if (worldX + FrameAsset.TILE_SIZE > gamePanel.player.getWorldX() - gamePanel.player.screenX) {
+
+            for (int i = 0; i < 8; i++) {
+                if (defaultImages[i] == null) {
+                    System.out.println("Image " + i + " = null");
+                }
+            }
+
+            if (image == null) {
+                image = defaultImages[6];
+            }
+
+            switch (direction) {
+                case UP -> image = imageChecker ? defaultImages[0] : defaultImages[1];
+                case DOWN -> image = imageChecker ? defaultImages[2] : defaultImages[3];
+                case LEFT -> image = imageChecker ? defaultImages[4] : defaultImages[5];
+                case RIGHT -> image = imageChecker ? defaultImages[6] : defaultImages[7];
+            }
+
+            if (isHpBarOn) {
+                double hpBarScale = (double) FrameAsset.TILE_SIZE / maxLife;
+                double hpBar = hpBarScale * life;
+
+                g2d.setColor(new Color(35, 35, 35));
+                g2d.fillRect(screenX - 1, screenY - 16, FrameAsset.TILE_SIZE + 2, 12);
+
+                g2d.setColor(new Color(255, 35, 35));
+                g2d.fillRect(screenX, screenY - 15, (int)hpBar, 10);
+            }
+
+            if (isInvincible) {
+                g2d.setComposite(AlphaComposite.getInstance( AlphaComposite.SRC_OVER, 0.4f));
+            }
+
+            g2d.drawImage(image, screenX, screenY, FrameAsset.TILE_SIZE, FrameAsset.TILE_SIZE, null);
+        }
     }
 }
