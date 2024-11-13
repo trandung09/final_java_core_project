610288a9fd8d36abc9427662475be2dd4d93006d
trandung09 <trandung09082004@gmail.com>
2024-11-13 19:41:20 +0700

    Create: setter class for item object.

---
+++ src/main/java/org/tvd/asset/ItemSetter.java
@@ -0,0 +1,12 @@
+package org.tvd.asset;
+
+import org.tvd.utility.UtilityTool;
+
+public class ItemSetter {
+
+    private static final UtilityTool utool = new UtilityTool();
+
+    public static void loadDefaultItemImage() {
+
+    }
+}
