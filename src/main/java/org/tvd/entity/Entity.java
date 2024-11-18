38b1d6d405eddca385276923b547c3eb58d3372d
trandung09 <trandung09082004@gmail.com>
2024-11-19 01:13:26 +0700

    Update: counter update and general update for entities.

--- src/main/java/org/tvd/entity/Entity.java
+++ src/main/java/org/tvd/entity/Entity.java
@@ -31,7 +31,6 @@ public abstract class Entity {
     protected boolean isCollisionOn;
     protected boolean isAttacking;
     protected boolean isInvincible;
-    protected boolean isDead;
     protected boolean isAlive = true;
     protected boolean imageChecker;
     protected boolean attackImageChecker;
@@ -81,35 +80,43 @@ public abstract class Entity {
 
         if (isAttacking) {
             counter.attackCounter++;
-
-            if (counter.attackCounter > 25) {
+            if (counter.attackCounter <= 5) {
+                attackImageChecker = true;
+            }
+            else if (counter.attackCounter <= 25) {
+                attackImageChecker = false;
+            }
+            else {
+                attackImageChecker = false;
                 isAttacking = false;
                 counter.attackCounter = 0;
             }
         }
 
-        if (!gamePanel.keyHandler.pressed.isMovePressed()) {
-            return;
+        if (isInvincible && counter.invincibleCounter++ >= 60) {
+            isInvincible = false;
+            counter.invincibleCounter = 0;
         }
 
-        if (counter.drawCounter++ > 10) {
-            counter.drawCounter = 0;
-        }
-    }
-
-    public void updateAnimationImage() {
-
-        if (counter.drawCounter == 10) {
-            imageChecker = !imageChecker;
-        }
-
-        if (isAttacking) {
-
-            attackImageChecker = counter.attackCounter <= 5;
+        if (isHpBarOn && counter.hpBarCounter++ >= 500) {
+            isHpBarOn = false;
+            counter.hpBarCounter = 0;
         }
     }
 
-    public abstract void update();
+    public void update() {
+
+        if (life <= 0) {
+            isAlive = false;
+        }
+
+        setAction();
+
+        isCollisionOn = false;
+
+        detection.checkCollisionWithTile(this);
+    }
+
     public abstract void setAction();
     public abstract void resetAction();
     public abstract void render(Graphics2D g2d);
