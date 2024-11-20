17cb538d3030e7fad0cd8e2ac129bf5c869c897e
trandung09 <trandung09082004@gmail.com>
2024-11-21 00:56:10 +0700

    Update: init method.

--- src/main/java/org/tvd/entity/monster/Orc.java
+++ src/main/java/org/tvd/entity/monster/Orc.java
@@ -1,8 +1,10 @@
 package org.tvd.entity.monster;
 
+import org.tvd.asset.EntitySetter;
+import org.tvd.entity.Direction;
 import org.tvd.frame.GamePanel;
 
-import java.awt.Graphics2D;
+import java.awt.*;
 
 public class Orc extends Monster {
 
@@ -18,18 +20,36 @@ public class Orc extends Monster {
         this.isAlive = true;
         this.maxLife = 15;
         this.life = maxLife;
-        this.speed = 4;
+        this.speed = 1;
+        this.experienceReward = 10;
         this.abilityAttack = true;
         this.abilityFly = false;
+        this.direction = Direction.RIGHT;
+        this.solidArea = new Rectangle(8, 8, 40, 40);
+        this.solidAreaDefaultX = solidArea.x;
+        this.solidAreaDefaultY = solidArea.y;
+        this.attackArea = new Rectangle(0, 0, 45, 45);
+
+        EntitySetter.loadDefaultEntityImage(this);
+        EntitySetter.loadAttackEntityImage(this, "attack");
+    }
+
+    public void update() {
+
+        super.update();
     }
 
     @Override
     public void setAction() {
 
+        super.setAction();
+
+        super.detectPlayerInSight(200, 80);
     }
 
     @Override
     public void render(Graphics2D g2d) {
 
+        super.render(g2d);
     }
 }
