b42ddcef3df8f5ffa1ebe15fa018256a1d56e26c
trandung09 <trandung09082004@gmail.com>
2024-11-22 03:55:44 +0700

    Update: method for init all item image.

--- src/main/java/org/tvd/asset/ItemSetter.java
+++ src/main/java/org/tvd/asset/ItemSetter.java
@@ -1,12 +1,16 @@
 package org.tvd.asset;
 
+import org.tvd.item.SuperItem;
 import org.tvd.utility.UtilityTool;
 
 public class ItemSetter {
 
     private static final UtilityTool utool = new UtilityTool();
 
-    public static void loadDefaultItemImage() {
+    public static void loadDefaultItemImage(SuperItem item) {
 
+        String path = "/item/" + item.getName();
+
+        item.setDefaultImage(utool.getImage(path, item.getImageSize(), item.getImageSize()));
     }
 }
