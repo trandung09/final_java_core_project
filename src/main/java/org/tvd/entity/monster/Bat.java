d81057288bf824a1a59b7f74305ad6f37774fe71
trandung09 <trandung09082004@gmail.com>
2024-11-23 19:46:48 +0700

    Update: super init method and set some private properties.

--- src/main/java/org/tvd/entity/monster/Bat.java
+++ src/main/java/org/tvd/entity/monster/Bat.java
@@ -1,7 +1,6 @@
 package org.tvd.entity.monster;
 
-import org.tvd.asset.EntitySetter;
-import org.tvd.entity.Direction;
+import org.tvd.setter.EntitySetter;
 import org.tvd.frame.GamePanel;
 
 public class Bat extends Monster {
@@ -13,14 +12,14 @@ public class Bat extends Monster {
         init();
     }
 
-    private void init() {
+    public void init() {
+
+        super.init();
 
         this.name = "bat";
         this.speed = 1;
-        this.direction = Direction.RIGHT;
         this.maxLife = 5;
         this.life = maxLife;
-        this.abilityAttack = false;
         this.abilityFly = true;
 
         EntitySetter.loadDefaultEntityImage(this);
