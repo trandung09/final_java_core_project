57fb05d0fbf95995ca436f414477a7212f8469ee
trandung09 <trandung09082004@gmail.com>
2024-11-23 19:54:06 +0700

    Update: override update, attacking method and attack action.

--- src/main/java/org/tvd/entity/monster/Slimey.java
+++ src/main/java/org/tvd/entity/monster/Slimey.java
@@ -1,33 +1,117 @@
 package org.tvd.entity.monster;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.tvd.entity.Direction;
+import org.tvd.entity.Projectile;
+import org.tvd.entity.Rock;
 import org.tvd.frame.GamePanel;
+import org.tvd.setter.EntitySetter;
+import org.tvd.setter.FrameAsset;
 
-import java.awt.Graphics2D;
-
+@Setter
+@Getter
 public class Slimey extends Monster {
 
     public Slimey(GamePanel gamePanel) {
+
         super(gamePanel);
 
         init();
     }
 
     public void init() {
+
+        super.init();
+
         this.name = "slimey";
-        this.speed = 2;
-        this.maxLife = 7;
+        this.speed = 1;
+        this.experienceReward = 8;
+        this.maxLife = 10;
         this.life = maxLife;
-        this.abilityAttack = true;
-        this.abilityFly = false;
+
+        EntitySetter.loadDefaultEntityImage(this);
+    }
+
+    @Override
+    public void update() {
+
+        if (life <= 0 ) {
+            isAlive = false;
+        }
+
+        setAction();
+
+        isCollisionOn = false;
+
+        detection.checkCollisionWithItems(this, gamePanel.itemManager);
+        detection.checkCollisionWithOtherEntity(this, gamePanel.player);
+        detection.checkCollisionWithOtherEntities(this, gamePanel.monsterManager);
+
+        detection.checkCollisionWithTile(this);
+
+        handleProjectileShooting();
+        attacking();
+
+        counter();
+        moving();
     }
 
     @Override
     public void setAction() {
 
+        super.setAction();
     }
 
     @Override
-    public void render(Graphics2D g2d) {
+    public void attacking() {
 
+       for (Projectile projectile : projectiles) {
+           projectile.setCollisionOn(false);
+           detection.checkCollisionWithOtherEntity(this, gamePanel.player);
+
+           if (projectile.isCollisionOn()) {
+               damagePlayer();
+           }
+       }
+    }
+
+    private void addProjectile() {
+
+        Projectile projectile = new Rock(gamePanel);
+        projectile.init(this, true);
+
+        projectiles.add(projectile);
+
+        counter.shootCounter = 0;
+    }
+
+    private void handleProjectileShooting() {
+
+        if (counter.shootCounter++ >= (int) 1e9) {
+            counter.shootCounter = 0;
+        }
+
+        int playerWorldX = gamePanel.player.getWorldX();
+        int playerWorldY = gamePanel.player.getWorldY();
+
+        int distanceX = Math.abs(playerWorldX - worldX);
+        int distanceY = Math.abs(playerWorldY - worldY);
+
+        if (distanceX < FrameAsset.TILE_SIZE) {
+            if ((playerWorldY > worldY && direction == Direction.DOWN) || (playerWorldY <= worldY && direction == Direction.UP)) {
+                if (counter.shootCounter > 300 && Math.abs(playerWorldY - worldY) <= 300) {
+                    addProjectile();
+                }
+            }
+        }
+
+        if (distanceY < FrameAsset.TILE_SIZE) {
+            if ((playerWorldX > worldX && direction == Direction.RIGHT || (playerWorldX <= worldX && direction == Direction.LEFT))) {
+                if (counter.shootCounter > 300 && Math.abs(playerWorldY - worldY) <= 300) {
+                    addProjectile();
+                }
+            }
+        }
     }
 }
