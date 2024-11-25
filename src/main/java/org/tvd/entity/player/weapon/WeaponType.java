44d8abb9a633f8b5e144b0336c41d470de49b0cb
trandung09 <trandung09082004@gmail.com>
2024-11-25 14:48:43 +0700

    Update: Add the weapon's possible damage property.

--- src/main/java/org/tvd/entity/player/weapon/WeaponType.java
+++ src/main/java/org/tvd/entity/player/weapon/WeaponType.java
@@ -1,5 +1,15 @@
 package org.tvd.entity.player.weapon;
 
+import lombok.Getter;
+
+@Getter
 public enum WeaponType {
-    AXE, PICK, SWORD
+
+    AXE(1), PICK(2), SWORD(3);
+
+    private final int damage;
+
+    WeaponType(int damage) {
+        this.damage = damage;
+    }
 }
