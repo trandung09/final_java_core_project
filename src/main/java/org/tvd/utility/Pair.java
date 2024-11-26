5156955de9909c37a02cb451c5553cab532611d0
trandung09 <trandung09082004@gmail.com>
2024-11-26 16:01:04 +0700

    Create: pair class.

---
+++ src/main/java/org/tvd/utility/Pair.java
@@ -0,0 +1,12 @@
+package org.tvd.utility;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Pair<K, V> {
+
+    public K first;
+    public V second;
+}
